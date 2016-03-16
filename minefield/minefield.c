0abdcf5893b5eda416ade9345a5ddaed56dbd0c4
Lucas Osse <lucaskindler@gmail.com>
2016-03-16 20:39:22 +0000

    Fixed 05.c printing and imaginary foot error

---
+++ minefield/minefield.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void printMenu(){
+  system("clear");
+  printf("\t\t====WELCOME TO MINEFIELD===\n");
+  printf("\t1.Start Game\n");
+  printf("\t2.??\n");
+  printf("\t3.Exit");
+  printf("Type Option: ");
+
+
+
+}
