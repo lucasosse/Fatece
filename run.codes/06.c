c4029fb1122b178f45bc6eb5417d3b1dd33e036a
Lucas-Ubuntu <lucaskindler@gmail.com>
2016-03-23 20:45:03 -0300

    First stable of 06

--- run.codes/06.c
+++ run.codes/06.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main(int argc, char *argv[]){
 
-  scanf("%f %f");
+int i, pairCount=0;
+float p[50], q[50], res;
+
+  scanf("%d", &pairCount);
+
+  for(i=0;i<pairCount;++i){
+    scanf("%f %f", &p[i], &q[i]);
+    if(i=0){
+      res = sqrt(pow(p[i]-q[i], 2));
+    }else{
+      res = res+sqrt(pow(p[i]-q[i], 2));
+    }
+    i++;
+  }
+
+  printf("%.4f\n", res);
 
 return 0;
 }
