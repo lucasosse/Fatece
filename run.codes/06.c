83261e6d270aa132f3ea7403afe0ab7d9aee2fc7
Lucas-Ubuntu <lucaskindler@gmail.com>
2016-03-23 21:09:43 -0300

    fixed loop on 06

--- run.codes/06.c
+++ run.codes/06.c
@@ -5,18 +5,27 @@
 int main(int argc, char *argv[]){
 
 int i, pairCount=0;
-float p[50], q[50], res;
+float p[50], q[50];
+float res = 0;
 
   scanf("%d", &pairCount);
 
-  for(i=0;i<pairCount;++i){
-    scanf("%f %f", &p[i], &q[i]);
-    if(i=0){
+  for(i=0;i<pairCount;i++){
+
+    if(i==0){
+      scanf("%f %f", &p[i], &q[i]);
+      //printf("passed scan\n");
       res = sqrt(pow(p[i]-q[i], 2));
+      //printf("passed equation, result: %f",res);
+      //i++;
+      //printf("passed incrementer");
     }else{
+      //printf("looped once");
+      scanf("%f %f", &p[i], &q[i]);
       res = res+sqrt(pow(p[i]-q[i], 2));
+      //i++;
     }
-    i++;
+
   }
 
   printf("%.4f\n", res);
